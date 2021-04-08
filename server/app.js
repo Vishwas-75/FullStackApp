d7db595328ed2707d752bac8f0a44411d9b6fc09
Vishwas-75 <vishwashnvishu75@gamil.com>
2021-04-09 03:03:21 +0530

    add cors

--- server/app.js
+++ server/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
-
-
+const cors =require('cors')
 
 const app = express()
- 
+
+//alllow cross origin request 
+app.use(cors())
 mongoose.connect('mongodb://localhost/fullStackApp', { useNewUrlParser: true , useUnifiedTopology: true , useCreateIndex: true})
 mongoose.set("useCreateIndex", true)
 mongoose.connection.once('open',()=>

71e0294f000ac4c7bd66c011dbc1cb13d5c312d3
Vishwas-75 <vishwashnvishu75@gamil.com>
2021-04-09 03:02:48 +0530

    write frist query and get output in browser

--- client/src/Components/BookList.jsx
+++ client/src/Components/BookList.jsx
@@ -1,31 +1,43 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+// import  gql  from 'graphql-tag';
+// import {compose, graphql} from '@apollo/client';
+import {gql, useQuery} from '@apollo/client'
 
-const BookListContainer=styled.div`
+const GET_ALL_BOOKS = gql `
+query {
+     allBooks{
+        name
+        bookId
+     }
+}
+`;
+
+const BookListContainer = styled.div `
  font-family:Century Gothic;
-
-
 `;
-const UnList=styled.ul`
+const UnList = styled.ul `
 `;
-const List=styled.li`
-
-
+const List = styled.li `
 `;
-const HeaderText=styled.h1`
-
-
+const HeaderText = styled.h1 `
 `;
 function BookList() {
+     const {data, loading, error } = useQuery(GET_ALL_BOOKS)
+     if (loading) return <p>Loading...</p>;
+     if (error) return <p>Error :(</p>;
+          console.log(error);
+     console.log(data);
+     const books=data.allBooks;
      return (
           <BookListContainer>
-            <HeaderText>Books</HeaderText>
+               <HeaderText>Books</HeaderText>
                <UnList>
-                    <List>Book Name</List>
+                 {books.map((book)=><List key={book.bookId}>{book.name}</List>)}
 
                </UnList>
           </BookListContainer>
      )
 }
 
-export default BookList
+export default BookList;
